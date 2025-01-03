c6a2ba22b3c09442d24933b840ded4e918dbfeb3
Adriano Froes da Costa <adriano.costa@softplan.com.br>
2025-01-03 19:11:43 -0300

    Initial commit

---
+++ API-SANTANDER/src/test/java/com/froes/apisantander/ApiSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.froes.apisantander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
